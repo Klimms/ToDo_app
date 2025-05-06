ef8b93c42517f52ebbb9ff08cebdec16dec959cc
Klimm <aleks.s.klimov@gmail.com>
2025-05-06 07:49:54 +0300

    add

--- models.py
+++ models.py
@@ -1,11 +1,11 @@
-from sqlalchemy import BigInteger, DateTime, String, Float, ForeignKey, Integer
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from sqlalchemy import ForeignKey, String, BigInteger
+from sqlalchemy.orm import Mapped, DeclarativeBase, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
 
-engine = create_async_engine(url="sqlite+aiosqlite:///db_todo.sqlite3", echo=True)
+engine = create_async_engine(url='sqlite+aiosqlite:///db.sqlite3', echo=True)
 
-async_session = async_sessionmaker(engine, expire_on_commit=False)
+async_session = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 
 class Base(AsyncAttrs, DeclarativeBase):
@@ -13,23 +13,22 @@ class Base(AsyncAttrs, DeclarativeBase):
 
 
 class User(Base):
-    __tablename__ = "users"
-
+    __tablename__ = 'users'
+    
     id: Mapped[int] = mapped_column(primary_key=True)
-    tg_id: Mapped[int] = mapped_column(BigInteger)
-    username: Mapped[str] = mapped_column(String(32))
-    full_name: Mapped[str] = mapped_column(String(64))
-    
-    
+    tg_id = mapped_column(BigInteger)
+
+
 class Task(Base):
-    __tablename__ = "tasks"
-
+    __tablename__ = 'tasks'
+    
     id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(String(512))
-    competed: Mapped[bool] = mapped_column(default=False)
-    user: Mapped[int] = mapped_column(ForeignKey("users.tg_id", ondelete="CASCADE"))
-    
-    
+    title: Mapped[str] = mapped_column(String(128))
+    completed: Mapped[bool] = mapped_column(default=False)
+    user: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'))
+
+
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+        
--- requests.py
+++ requests.py
@@ -7,13 +7,13 @@ from typing import List
 class TaskSchema(BaseModel):
     id: int
     title: str
-    compited: bool
+    completed: bool
     user: int
-
+    
     model_config = ConfigDict(from_attributes=True)
-    
-    
-async def add_user(tg_id: int):
+
+
+async def add_user(tg_id):
     async with async_session() as session:
         user = await session.scalar(select(User).where(User.tg_id == tg_id))
         if user:
@@ -21,23 +21,41 @@ async def add_user(tg_id: int):
         
         new_user = User(tg_id=tg_id)
         session.add(new_user)
-        session.commit()
-        session.refresh(new_user)
+        await session.commit()
+        await session.refresh(new_user)
         return new_user
-    
 
-async def get_task(user_id):
+
+async def get_tasks(user_id):
     async with async_session() as session:
         tasks = await session.scalars(
-            select(Task).where(Task.user == user_id, Task.compited == False)
-            )
-        serialazed_tasks = [
+            select(Task).where(Task.user == user_id, Task.completed == False)
+        )
+        
+        serialized_tasks = [
             TaskSchema.model_validate(t).model_dump() for t in tasks
         ]
         
-        return serialazed_tasks
-        
-        
+        return serialized_tasks
+
+
 async def get_completed_tasks_count(user_id):
     async with async_session() as session:
-        return await session.scalar(select(func.count(Task.id)).where(Task.competed == True))
+        return await session.scalar(select(func.count(Task.id)).where(Task.completed == True))
+
+
+async def add_task(user_id, title):
+    async with async_session() as session:
+        new_task = Task(
+            title=title,
+            user=user_id
+        )
+        session.add(new_task)
+        await session.commit()
+
+
+async def update_task(task_id):
+    async with async_session() as session:
+        await session.execute(update(Task).where(Task.id == task_id).values(completed=True))
+        await session.commit()
+        
